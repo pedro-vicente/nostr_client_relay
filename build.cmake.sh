83de56d99abe86a9b8cc00f11ded23009dbf9b3a
randymcmillan <randy.lee.mcmillan@gmail.com>
2023-07-19 23:04:49 -0400

    build.cmake.sh: detect vostro on port -p

--- build.cmake.sh
+++ build.cmake.sh
@@ -33,12 +33,35 @@ export WX_PREFIX
 
 sleep 3
 
+#echo ${!#}
+if [[ $1 == "-p" ]];then
+PORT=${!#}
+#echo $PORT
+fi
+
+VOSTRO=$(lsof -i -n -P | grep $PORT | grep vostro)
+if [[ "$VOSTRO" == *"vostro"* ]]; then
+	echo "vostro already using port:$PORT" && exit;
+fi
+##absolute path of script
+SCRIPT=$(realpath -s "$0")
+##directory script is in
+SCRIPTPATH=$(dirname "$SCRIPT")
+SCRIPT_PATH="${BASH_SOURCE}"
+#echo $SCRIPT
+echo $SCRIPTPATH
+#echo $SCRIP_TPATH
+
+WX_PREFIX=$($(which wx-config) --prefix)
+export WX_PREFIX
+
 if [[ "$OSTYPE" == "msys" ]]; then
 dir=$PWD
 fi
 
 mkdir -p build
 pushd build
+## <<<<<<< HEAD
 ## <<<<<<< HEAD
 ## if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 ##
@@ -56,11 +79,26 @@ cmake .. -DWT_INCLUDE="${WX_PREFIX}/lib/include" -DWT_CONFIG_H="${WX_PREFIX}/inc
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 
 cmake .. -DWT_INCLUDE="${WX_PREFIX}/lib/include" -DWT_CONFIG_H="${WX_PREFIX}/include" -DBUILD_WEB=$opt -DBUILD_GUI=$opt
+## =======
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+
+cmake .. -DWT_INCLUDE="${WX_PREFIX}/lib/include" -DWT_CONFIG_H="${WX_PREFIX}/include" -DBUILD_WEB=ON -DBUILD_GUI=ON
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+
+cmake .. -DWT_INCLUDE="${WX_PREFIX}/lib/include" -DWT_CONFIG_H="${WX_PREFIX}/include" #-DBUILD_WEB=ON #-DBUILD_GUI=ON
+## >>>>>>> 87be3f46 (build.cmake.sh: detect vostro on port -p)
 
 elif [[ "$OSTYPE" == "msys" ]]; then
 
 cmake .. --fresh -DBUILD_STATIC=OFF -DWT_INCLUDE="$dir/ext/wt-4.10.0/src" -DWT_CONFIG_H="$dir/ext/wt-4.10.0/build" \
+## <<<<<<< HEAD
 -DBUILD_WEB=$opt -DBUILD_GUI=$opt
+## =======
+## -DBUILD_WEB=ON -DBUILD_GUI=ON
+##
+## >>>>>>> 87be3f46 (build.cmake.sh: detect vostro on port -p)
 cmake --build .
 
 fi
@@ -81,9 +119,12 @@ sleep 3
 cmake --build .
 
 popd
+<<<<<<< HEAD
 ## <<<<<<< HEAD
 ## pwd
 ## =======
+## =======
+## >>>>>>> 87be3f46 (build.cmake.sh: detect vostro on port -p)
 echo `pwd`
 
 pushd build
@@ -105,12 +146,21 @@ if [ "$remote" == "https://github.com/pedro-vicente/nostr_client_relay.git" ]; t
 ## =======
 	export LD_LIBRARY_PATH="$HOME/github/nostr_client_relay/ext/boost_1_82_0/stage/lib":$LD_LIBRARY_PATH
 else
+## <<<<<<< HEAD
 	export LD_LIBRARY_PATH="$SCRIPTPATH/ext/boost_1_82_0/stage/lib":$LD_LIBRARY_PATH
 fi
 if [[ "$OSTYPE" == "msys"* ]]; then
 	./Debug/vostro --http-address=0.0.0.0 --http-port=8080  --docroot=.
 else
 	pushd $SCRIPTPATH/build && ./vostro $port || echo "port busy?"
+## =======
+export LD_LIBRARY_PATH="$SCRIPTPATH/ext/boost_1_82_0/stage/lib":$LD_LIBRARY_PATH
+fi
+if [[ "$OSTYPE" == "msys"* ]]; then
+./Debug/vostro --http-address=0.0.0.0 --http-port=8080  --docroot=.
+else
+$SCRIPTPATH/build/./vostro --http-address=0.0.0.0 --http-port=$PORT  --docroot=. || echo "port busy?"
+## >>>>>>> 87be3f46 (build.cmake.sh: detect vostro on port -p)
 fi
 
 exit
