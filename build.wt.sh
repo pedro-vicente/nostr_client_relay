eee2cdd2653855a3496fa29098acbd3a90b90330
randymcmillan <randy.lee.mcmillan@gmail.com>
2023-07-21 00:56:59 -0400

    ext/wt-4.10.0/CMakeLists.txt: include_directories(../openssl-3.0.5)
    modified:   CMakeLists.txt
    modified:   build.wt.sh
    modified:   ext/wt-4.10.0/CMakeLists.txt

--- build.wt.sh
+++ build.wt.sh
@@ -25,6 +25,7 @@ cmake --install . --config Debug
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 
 ## <<<<<<< HEAD
+## <<<<<<< HEAD
 ## cmake .. -DCMAKE_INSTALL_PREFIX=/Users/pvn/wt_install -DBOOST_PREFIX="$HOME/git/nostr_client_relay/ext/boost_1_82_0" -DINSTALL_EXAMPLES=ON
 ## cmake --build . --parallel 9
 ## make install
@@ -33,6 +34,8 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 ##
 ## cmake .. -DCMAKE_INSTALL_PREFIX="$HOME/wt_install" -DBOOST_PREFIX="$HOME/git/nostr_client_relay/ext/boost_1_82_0" \
 ## =======
+## =======
+## >>>>>>> e5f272ab (ext/wt-4.10.0/CMakeLists.txt: include_directories(../openssl-3.0.5))
 cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local -DBOOST_PREFIX="$PWD/ext/boost_1_82_0" -DINSTALL_EXAMPLES=ON
 cmake --build . --parallel 9
 make install
@@ -40,7 +43,10 @@ make install
 elif [[ "$OSTYPE" == "linux-gnu"* ]] || [[ "$OSTYPE" == "Darwin" ]]; then
 
 cmake .. -DCMAKE_INSTALL_PREFIX="/usr/local" -DBOOST_PREFIX="$PWD/ext/boost_1_82_0" \
+## <<<<<<< HEAD
 ## >>>>>>> 559c1748 (Initial commit)
+## =======
+## >>>>>>> e5f272ab (ext/wt-4.10.0/CMakeLists.txt: include_directories(../openssl-3.0.5))
 -DBUILD_EXAMPLES=OFF -DENABLE_HARU=OFF -DENABLE_PANGO=OFF -DENABLE_POSTGRES=OFF -DENABLE_FIREBIRD=OFF -DENABLE_MYSQL=OFF -DENABLE_MSSQLSERVER=OFF \
 -DENABLE_QT4=OFF -DENABLE_QT5=OFF -DENABLE_QT6=OFF -DENABLE_LIBWTTEST=OFF -DENABLE_LIBWTDBO=OFF -DENABLE_OPENGL=OFF
 cmake --build . --parallel 1
